81b37659d65d471c5eb3f3d9e3885640815841e5
41기 조윤환 <114235846+jyh2610@users.noreply.github.com>
2023-05-20 20:46:50 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,16 +1,13 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
+const rel = []
 
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
-        }
-    }
-
-    return answer;
+let count =0
+while(progresses.map(item => item >100){ 
+   count ++
+   progresses = progresses.map((item , i )=>{
+        item = item + speeds[i]
+    })
+   
+}
+    return rel
 }
