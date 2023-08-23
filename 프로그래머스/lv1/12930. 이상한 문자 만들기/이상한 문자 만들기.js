b4e3b03525368a69215a3e20ebbb20436badc60d
조윤환 <114235846+jyh2610@users.noreply.github.com>
2023-08-24 00:14:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    const toArr = s.split(' ')
+    const arr = toArr.map(item =>{
+        const data = item.split('')
+       return  data.map((item,i)=>{
+            if(i === 0 || i%2 === 0){
+               return item.toUpperCase()
+            }else{
+               return item.toLowerCase()
+            }
+        })
+ 
+    })
+const result = arr.map(subArray => subArray.join("")).join(" ");
+
+return result
+}
