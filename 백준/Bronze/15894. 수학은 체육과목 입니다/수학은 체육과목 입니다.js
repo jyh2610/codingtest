af97432d5aaa589c5d3636fa031ba9200525e7df
jyh2610 <114235846+jyh2610@users.noreply.github.com>
2024-01-07 17:51:24 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
@@ -4,6 +4,6 @@ const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(file).toString().trim();
 
 const num = Number(input);
-const res = (n) => 3 * (n - 1) + n + 3;
+const res = (n) => 3 * n + n;
 
 console.log(res(num));
