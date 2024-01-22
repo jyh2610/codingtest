bc4a848f688fe8003f0529e1f6b44484d91d00ae
jyh2610 <114235846+jyh2610@users.noreply.github.com>
2024-01-22 20:59:32 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 168 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString();
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(input));
